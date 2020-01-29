becfe8c1b0125519e594a1e9105d078deba2ae47
Ryan Murray <rymurr@dremio.com>
2020-01-29 15:17:46 +0000

    fix docs

--- docs/conf.py
+++ docs/conf.py
@@ -21,11 +21,11 @@
 import os
 import sys
 
-import dremio_client
-
 
 sys.path.insert(0, os.path.abspath(".."))
 
+
+import dremio_client  # NOQA
 
 # -- General configuration ---------------------------------------------
 
