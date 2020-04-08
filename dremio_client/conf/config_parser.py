417c9cf87ed3febac7e261ec5a1975d2b282a99c
Ryan Murray <rymurr@dremio.com>
2020-04-08 17:36:55 +0200

    fix formatting

--- dremio_client/conf/config_parser.py
+++ dremio_client/conf/config_parser.py
@@ -33,9 +33,9 @@ def _get_env_args():
         if "DREMIO_" in k and k != "DREMIO_CLIENTDIR":
             name = k.replace("DREMIO_", "").lower().replace("_", ".")
             if name == "port":
-              v = int(v)
-            elif name=="ssl":
-              v = v.lower() in ['true', '1', 't', 'y', 'yes', 'yeah', 'yup', 'certainly', 'uh-huh']
+                v = int(v)
+            elif name == "ssl":
+                v = v.lower() in ["true", "1", "t", "y", "yes", "yeah", "yup", "certainly", "uh-huh"]
             args[name] = v
     return args
 
