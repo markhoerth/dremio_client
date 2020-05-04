566ee7d4c3fd433f702cab194ae5a9a047d46eb3
Ryan Murray <rymurr@dremio.com>
2020-05-04 21:33:56 +0200

    Bump version: 0.12.0 → 0.12.1

--- setup.py
+++ setup.py
@@ -82,7 +82,7 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/rymurr/dremio_client",
-    version="0.12.0",
+    version="0.12.1",
     zip_safe=False,
     extras_require={
         ':python_version == "2.7"': ["futures"],
