b8d599af218239c502d3dab17fe140a672d28a90
viktordremio <viktor@dremio.com>
2020-05-09 20:16:36 +0200

    refresh dataset reflection through disable and enable flag

--- dremio_client/util/__init__.py
+++ dremio_client/util/__init__.py
@@ -25,7 +25,8 @@
 
 from .query import refresh_metadata, run, run_async
 from .promote import promote_catalog
-from .refresh import refresh_vds_reflection_by_path
+from .refresh import refresh_vds_reflection_by_path, refresh_reflections_of_one_dataset
 
 
-__all__ = ["run", "run_async", "refresh_metadata", "promote_catalog", "refresh_vds_reflection_by_path"]
+__all__ = ["run", "run_async", "refresh_metadata", "promote_catalog",
+           "refresh_vds_reflection_by_path", "refresh_reflections_of_one_dataset"]
--- dremio_client/util/refresh.py
+++ dremio_client/util/refresh.py
@@ -1,4 +1,7 @@
 
+import json
+
+
 def refresh_vds_reflection_by_path(client, path=None):
     """
     By providing a path from VDS the reflection of that vds will be refreshed
@@ -9,16 +12,16 @@ def refresh_vds_reflection_by_path(client, path=None):
     """
     datasets = []
     datasets.append(client.catalog_item(cid=None, path=path))
-    refresh_by_path(client, datasets)
+    _refresh_by_path(client, datasets)
 
 
-def refresh_by_path(client, datasets):
+def _refresh_by_path(client, datasets):
     for dataset in datasets:
         response = client.graph(dataset['id'])
-        parents(client, response['parents'])
+        _parents(client, response['parents'])
 
 
-def parents(client, parents):
+def _parents(client, parents):
     datasets = []
     for parent in parents:
         if (parent['datasetType'] == 'VIRTUAL'):
@@ -26,4 +29,49 @@ def parents(client, parents):
         else:
             client.refresh_pds(client.catalog_item(cid=parent['id'], path=None)['id'])
         break
-    refresh_by_path(client, datasets)
+    _refresh_by_path(client, datasets)
+
+
+def _reenable_reflection(client, refl_json):
+    data = refl_json
+
+    key_to_remove = ['updatedAt', 'createdAt',
+                     'currentSizeBytes', 'totalSizeBytes']
+    for key in key_to_remove:
+        data.pop(key)
+
+    refl_id = data['id']
+    data['enabled'] = False
+    data = json.dumps(data)
+
+    # disable reflesction
+    client.modify_reflection(refl_id, data)
+
+    # get new tag version for update
+
+    data = client.reflection(refl_id)
+    #data = res.json()
+
+    for key in key_to_remove:
+        data.pop(key)
+    data['enabled'] = True
+    data = json.dumps(data)
+
+    # enable reflection, will trigger an update
+    print(client.modify_reflection(refl_id, data)['enabled'])
+
+
+def refresh_reflections_of_one_dataset(client, path=None):
+    """
+    By providing a path from dataset the reflection of that dataset will be refreshed through reenable
+    Enebled reflections of dataset and all reflections from derived dataset will be refreshed
+
+    :param path: list ['space', 'folder', 'vds']
+    """
+    reflections = client.reflections()
+    dataset_id = client.catalog_item(cid=None, path=path)['id']
+
+    for reflection in reflections['data']:
+        if dataset_id == reflection['datasetId']:
+            if reflection['enabled'] == True:
+                _reenable_reflection(client, reflection)
