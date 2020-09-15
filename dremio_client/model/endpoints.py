b0e9a74be5b2293b7aef8dc88f3dfb23d4c9f69d
kishore.kumar <kishore.kumar@dremio.com>
2020-09-15 22:52:05 +0530

    addressing review comments

--- dremio_client/model/endpoints.py
+++ dremio_client/model/endpoints.py
@@ -481,21 +481,22 @@ def set_personal_access_token(token, base_url, uid, label, lifetime=24, ssl_veri
     )
 
 
-def delete_personal_access_token(token, base_url, uid, tid=None, ssl_verify=True):
-    """ delete a pat for a given user
+def delete_personal_access_token(token, base_url, uid=None, tid=None, ssl_verify=True):
+    """ delete a personal access token.
 
     https://docs.dremio.com/rest-api/user/delete-user-uid-token.html
+    https://docs.dremio.com/rest-api/token/
 
     :param token: auth token
     :param base_url: sql query
-    :param uid: id user
+    :param uid: id user (optional)
     :param tid: label of token (optional)
     :param ssl_verify: ignore ssl errors if False
     :return: updated catalog entity
     """
-    return _delete(
-        base_url + "/api/v3/user/{}/token{}".format(uid, ("/" + tid) if tid else ""), token, ssl_verify=ssl_verify
-    )
+    url_user_component = "user/{}/".format(uid) if uid else ""
+    url = base_url + "/api/v3/{}token{}".format(url_user_component, ("/" + tid) if tid else "")
+    return _delete(url, token, ssl_verify=ssl_verify)
 
 
 def modify_reflection(token, base_url, reflectionid, json, ssl_verify=True):
@@ -654,13 +655,4 @@ def graph(token, base_url, cid=None, ssl_verify=True):
     if cid is None:
         raise TypeError("resource id can't be None for a graph call")
     return _get(base_url + "/api/v3/catalog/{}/graph".format(cid), token, ssl_verify=ssl_verify)
-
-def delete_personal_access_token_without_userid(token, base_url, tid=None, ssl_verify=True):
-    """ Deletes token(s). Similar to delete_personal_access_token but this doesn't need user-id
-
-    https://docs.dremio.com/rest-api/token/
-    :param tid: token id to delete a specific token. None to delete all.
-    :return: None
-    """
-    url = base_url + "/api/v3/token{}".format(("/" + tid) if tid else "")
-    return _delete(url, token, url, ssl_verify=ssl_verify)
+	
